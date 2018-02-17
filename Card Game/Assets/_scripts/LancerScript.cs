b92d0686b6dfffcd5c382c00832971010941f2a8
Thanh Trinh <trinhtania@gmail.com>
2018-02-16 20:12:00 -0600

    add LancerScript to prefab
    add code to LancerScript (doesn’t do what it’s intended to)
    
    LancerScript for now is supposed to print the lancer’s HP in the
    console, which isn’t working at the moment.

--- Card Game/Assets/_scripts/LancerScript.cs
+++ Card Game/Assets/_scripts/LancerScript.cs
@@ -1,16 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Data;
 
 public class LancerScript : MonoBehaviour {
 
+	private GameObject soldier;
+	private DatabaseManager db;
+
+	IDataReader reader;
+
+	string name;
+	int mana;
+	int atk_dmg;
+	int hp;
+	int movement;
+	int range;
+	string description;
+
 	// Use this for initialization
 	void Start () {
-		
+		db = soldier.GetComponent<DatabaseManager> ();
+		reader.Read ();
+
+		name = reader.GetString (0);
+		mana = reader.GetInt32 (1);
+		atk_dmg = reader.GetInt32 (2);
+		hp = reader.GetInt32 (3);
+		movement = reader.GetInt32 (4);
+		range = reader.GetInt32 (5);
+		description = reader.GetString (6);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	public void getHP() {
+		Debug.Log ("HP = "+ hp);
 	}
 }

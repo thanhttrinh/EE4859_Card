a40cd0544dba09abc044939f8d5e138bbdaeffa7
Thanh Trinh <trinhtania@gmail.com>
2018-02-19 17:10:35 -0600

    add new scripts to test using classes instead of databse

---
+++ Card Game/Assets/_scripts/Card.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour {
+
+	public string name;
+	public int mana;
+
+	public Card() {
+		name = "";
+		mana = 0;
+	}
+
+	public Card(string n, int m) {
+		name = n;
+		mana = m;
+	}
+
+	public string getName() {
+		return name;
+	}
+
+	public int getMana() {
+		return mana;
+	}
+}

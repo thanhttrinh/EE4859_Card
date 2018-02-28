673e8fbab3c27f01ac5d37d220c7c9df7446e11a
Thanh Trinh <trinhtania@gmail.com>
2018-02-27 21:20:21 -0600

    add ManaVisual.cs to update mana text in game layout

---
+++ Card Game/Assets/_scripts/ManaVisual.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class ManaVisual : MonoBehaviour {
+
+    public int TestFullCrystals;
+    public int TestTotalCrystalsThisTurn;
+
+    //public Image[] Crystals;
+    public Text ProgressText;
+
+    private int totalCrystals;
+
+    public int TotalCrystals
+    {
+        get { return totalCrystals; }
+
+        set
+        {
+            //Debug.Log("Changed total mana to: " + value);
+
+            if (value > totalCrystals)
+                totalCrystals = totalCrystals;
+            else if (value < 0)
+                totalCrystals = 0;
+            else
+                totalCrystals = value;
+
+            // update the text
+            ProgressText.text = string.Format("{0}/{1}", availableCrystals.ToString(), totalCrystals.ToString());
+        }
+    }
+
+    private int availableCrystals;
+    public int AvailableCrystals
+    {
+        get { return availableCrystals; }
+
+        set
+        {
+            //Debug.Log("Changed mana this turn to: " + value);
+
+            if (value > totalCrystals)
+                availableCrystals = totalCrystals;
+            else if (value < 0)
+                availableCrystals = 0;
+            else
+                availableCrystals = value;
+
+            // update the text
+            ProgressText.text = string.Format("{0}/{1}", availableCrystals.ToString(), totalCrystals.ToString());
+
+        }
+    }
+
+    void Update()
+    {
+        if (Application.isEditor && !Application.isPlaying)
+        {
+            TotalCrystals = TestTotalCrystalsThisTurn;
+            AvailableCrystals = TestFullCrystals;
+        }
+    }
+
+}

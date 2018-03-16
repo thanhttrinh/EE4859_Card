bad25ee30311e088cffc5911531e229fd673cdd2
Thanh Trinh <trinhtania@gmail.com>
2018-03-16 14:19:53 -0500

    add SpellPreview.cs and add script to SpellCard prefab

---
+++ Card Game/Assets/_scripts/_card_scripts/SpellPreview.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class SpellPreview : MonoBehaviour
+{
+    public GameObject PreviewUnit;
+
+    public Text PreviewText;
+    public Text NameText;
+    public Text ManaText;
+    public Text RangeText;
+    public Text DescriptionText;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (SceneManager.GetActiveScene().name == "InGame")
+        {
+            PreviewText.text = string.Format("Name = {0}\nMana = {1}\nRange = {2}\nDescription = {3}", NameText.text, ManaText.text, RangeText.text, DescriptionText.text);
+            PreviewUnit.SetActive(true);
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (SceneManager.GetActiveScene().name == "InGame")
+            PreviewUnit.SetActive(false);
+    }
+
+    public void OnMouseEnter()
+    {
+        if (SceneManager.GetActiveScene().name == "InGame")
+        {
+            PreviewText.text = string.Format("Name = {0}\nMana = {1}\nRange = {2}\nDescription = {3}", NameText.text, ManaText.text, RangeText.text, DescriptionText.text);
+            PreviewUnit.SetActive(true);
+        }
+    }
+
+    public void OnMouseExit()
+    {
+        if (SceneManager.GetActiveScene().name == "InGame")
+            PreviewUnit.SetActive(false);
+    }
+}

db7c801976e6ba768979306fea9988f5c275516d
Thanh Trinh <trinhtania@gmail.com>
2018-03-16 14:08:42 -0500

    add OneCropManager.cs to Crop prefab and OneSoldierManager.cs to Soldier prefab
    - create OneCropManager.cs and OneSoldierManager.cs
    - remove OneCardManager.cs from Crop and Soldier prefabs

---
+++ Card Game/Assets/_scripts/_card_scripts/OneCropManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OneCropManager : MonoBehaviour
+{
+    public CardAsset cardAsset;
+    public OneCardManager PreviewManager;
+    [Header("Text Component References")]
+    public Text HealthText;
+    public Text CropSizeText;
+    [Header("Image References")]
+    public Image CreatureGraphicImage;
+
+    private int hp;
+
+    void Awake()
+    {
+        if (cardAsset != null)
+            ReadCreatureFromAsset();
+    }
+
+    public void ReadCreatureFromAsset()
+    {
+        // Change the card graphic sprite
+        CreatureGraphicImage.sprite = cardAsset.CardImage;
+
+        HealthText.text = cardAsset.CropHealth.ToString();
+        CropSizeText.text = cardAsset.CropSize.ToString();
+
+        if (PreviewManager != null)
+        {
+            PreviewManager.cardAsset = cardAsset;
+            PreviewManager.ReadCardFromAsset();
+        }
+
+    }
+
+    public void TakeDamage(int amount)
+    {
+        int hpAfter;
+        if (amount > 0)
+        {
+            int.TryParse(HealthText.text, out hp);
+            hpAfter = hp - amount;
+            HealthText.text = hpAfter.ToString();
+        }
+    }
+}
---
+++ Card Game/Assets/_scripts/_card_scripts/OneSoldierManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OneSoldierManager : MonoBehaviour
+{
+    public CardAsset cardAsset;
+    public OneCardManager PreviewManager;
+    [Header("Text Component References")]
+    public Text HealthText;
+    public Text AttackText;
+    public Text RangeText;
+    public Text MovementText;
+    [Header("Image References")]
+    public Image CreatureGraphicImage;
+
+    private int hp;
+
+    void Awake()
+    {
+        if (cardAsset != null)
+            ReadCreatureFromAsset();
+    }
+
+    public void ReadCreatureFromAsset()
+    {
+        // Change the card graphic sprite
+        CreatureGraphicImage.sprite = cardAsset.CardImage;
+
+        AttackText.text = cardAsset.Attack.ToString();
+        HealthText.text = cardAsset.MaxHealth.ToString();
+        RangeText.text = cardAsset.SoldierRange.ToString();
+        MovementText.text = cardAsset.Movement.ToString();
+
+        if (PreviewManager != null)
+        {
+            PreviewManager.cardAsset = cardAsset;
+            PreviewManager.ReadCardFromAsset();
+        }
+
+    }
+
+    public void TakeDamage(int amount)
+    {
+        int hpAfter;
+        if (amount > 0)
+        {
+            int.TryParse(HealthText.text, out hp);
+            hpAfter = hp - amount;
+            HealthText.text = hpAfter.ToString();
+        }
+    }
+}

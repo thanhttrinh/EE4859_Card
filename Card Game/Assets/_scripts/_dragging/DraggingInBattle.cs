e9f36e78471269af3c29d12fff6eb7b9e73f31df
Thanh Trinh <trinhtania@gmail.com>
2018-03-17 18:25:50 -0500

    create new dragging script to work on attacking/moving soldiers
    - added dragging scripts to Soldier prefab
    - also soldier prefab was rotated to 0.21 for some reason, changed it
    back to 0

---
+++ Card Game/Assets/_scripts/_dragging/DraggingInBattle.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class DraggingInBattle : DraggingActions 
+{
+	// script used to drag soldier to move/attack
+
+	private Vector3 savedPos;
+	private bool canAttack;
+	private bool canMove;
+
+	public override void OnStartDrag(){
+		savedPos = transform.position;
+	}
+
+	public override void OnEndDrag(){
+		transform.DOMove (savedPos, 0.1f);
+		//transform.DOMove (savedPos, 1f).SetEase (Ease.OutBounce, 0.5f, 0.1f);
+		//transform.DOMove (savedPos, 1f).SetEase (Ease.OutQuint, 0.5f, 0.1f);
+	}
+
+	public override void OnDraggingInUpdate(){
+
+	}
+
+	protected override bool DragSuccessful(){
+		return true;
+	}
+}

b7a5bad8bbd0358905ec03a6ac1bb050ab8024e2
Thanh Trinh <trinhtania@gmail.com>
2018-03-17 20:50:02 -0500

    add to DraggingInBattle.cs
    - trying to get raycasting onto another prefab to work
    - wondering whether to use dragging or mouse clicking for the gameplay

--- Card Game/Assets/_scripts/_dragging/DraggingInBattle.cs
+++ Card Game/Assets/_scripts/_dragging/DraggingInBattle.cs
@@ -8,17 +8,27 @@ public class DraggingInBattle : DraggingActions
 	// script used to drag soldier to move/attack
 
 	private Vector3 savedPos;
+	private Vector3 attackingPos;
 	private bool canAttack;
 	private bool canMove;
+	private RaycastHit hit;
+
+	void Start() {
+		canAttack = true;
+		canMove = true;
+	}
 
 	public override void OnStartDrag(){
 		savedPos = transform.position;
+
 	}
 
 	public override void OnEndDrag(){
+		attackingPos = transform.position + new Vector3 (0, 0, 0.5f);
+		Ray attacking = new Ray (attackingPos, Vector3.back);
+		if(Physics.Raycast(attacking, out hit, 2))
+			Debug.Log ("Something is in front of me.");
 		transform.DOMove (savedPos, 0.1f);
-		//transform.DOMove (savedPos, 1f).SetEase (Ease.OutBounce, 0.5f, 0.1f);
-		//transform.DOMove (savedPos, 1f).SetEase (Ease.OutQuint, 0.5f, 0.1f);
 	}
 
 	public override void OnDraggingInUpdate(){

f94540befeaca9ecbbce8d2e76e27924d70ca7f0
tle83 <tnle833@gmail.com>
2018-03-17 20:23:41 -0500

    update card collection with new card front

---
+++ Card Game/Assets/_scripts/_card_scripts/DeckSelectionScreen.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeckSelectionScreen : MonoBehaviour {
+
+	public GameObject screenContent;
+	public GameObject DeckIcons;
+	public GameObject title;
+
+	public static DeckSelectionScreen Instance;
+
+	void Awake()
+	{
+		Instance = this;
+		HideScreen ();
+	}
+
+	public void ShowDecks(){
+		if (DeckStorage.Instance.AllDecks.Count == 0) {
+			HideScreen ();
+			return;
+		}
+
+	}
+	public void HideScreen(){
+		screenContent.SetActive (false);
+		title.SetActive (true);
+	}
+}

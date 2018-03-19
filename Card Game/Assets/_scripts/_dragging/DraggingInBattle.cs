30567722fc4bfc68b8b15377957a265e57f06649
Thanh Trinh <trinhtania@gmail.com>
2018-03-19 18:08:08 -0500

    trying to get DragInBattle.cs to work with soldier movement

--- Card Game/Assets/_scripts/_dragging/DraggingInBattle.cs
+++ Card Game/Assets/_scripts/_dragging/DraggingInBattle.cs
@@ -7,11 +7,10 @@ public class DraggingInBattle : MonoBehaviour
 {
 	// script used to drag soldier to move/attack
 
-	private Vector3 savedPos;
-	private Vector3 attackingPos;
+	private int clickCounter = 4;
 	private bool canAttack;
 	private bool canMove;
-	private RaycastHit hit;
+	private int move;
 
     private void OnMouseUp()
     {
@@ -19,8 +18,11 @@ public class DraggingInBattle : MonoBehaviour
     }
 
     private void OnMouseDown()
-    {
-        
+	{
+		canMove = true;
+		canAttack = true;
+		clickCounter--;
+		SoldierMove ();
     }
 
     private void SoldierMove()
@@ -28,6 +30,17 @@ public class DraggingInBattle : MonoBehaviour
         //once soldier gameobject is detected
         //soldier should move based on his movement parameter
         //soldier shouldnt move again until next turn so `canMove == false`
+
+		if (clickCounter != 0 && canMove == true) 
+		{
+			int.TryParse (this.gameObject.GetComponent<OneSoldierManager> ().MovementText.text, out move);
+			Vector3 positiveX = new Vector3 (this.gameObject.transform.position.x + move, this.gameObject.transform.position.y, this.gameObject.transform.position.z);
+			Vector3 negativeX = new Vector3 (this.gameObject.transform.position.x - move, this.gameObject.transform.position.y, this.gameObject.transform.position.z);
+			if (Input.mousePosition.x <= this.gameObject.transform.position.x + move || Input.mousePosition.x >= this.gameObject.transform.position.x - move) 
+			{
+				this.gameObject.transform.position = new Vector3((int)Input.mousePosition.x, this.gameObject.transform.position.y, this.gameObject.transform.position.z);
+			}
+		}
     }
 
 }

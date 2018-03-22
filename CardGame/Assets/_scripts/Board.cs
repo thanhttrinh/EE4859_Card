96a86af19f00e5115d4c03b0e5423d77fd51fd9f
Thanh Trinh <trinhtania@gmail.com>
2018-03-22 10:56:43 -0500

    work on dragging cards onto board

--- CardGame/Assets/_scripts/Board.cs
+++ CardGame/Assets/_scripts/Board.cs
@@ -2,8 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Board : MonoBehaviour {
-
+public class Board : MonoBehaviour 
+{
+	
 	// Use this for initialization
 	void Start () {
 		
@@ -12,5 +13,14 @@ public class Board : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	void OnTriggerEnter(Collider collider)
+	{
+		//Debug.Log ("collision detected");
+		if (collider.gameObject.tag == "card") 
+		{
+			Destroy (collider.gameObject);
+		}
 	}
 }

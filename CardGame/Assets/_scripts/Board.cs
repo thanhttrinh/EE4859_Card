c04c23d04cdfeb45eb1004f7c0a692a788b9a47d
Thanh Trinh <trinhtania@gmail.com>
2018-04-02 18:15:59 -0500

    change InGame scene
    - change camera position

--- CardGame/Assets/_scripts/Board.cs
+++ CardGame/Assets/_scripts/Board.cs
@@ -6,6 +6,14 @@ public class Board : MonoBehaviour
 {
 	public OneSoldierManager[,] soldiers = new OneSoldierManager[6,6];
 	public OneCropManager[,] crops = new OneCropManager[6,6];
+
+	private Vector2 Tiles;
+	public GameObject BlueTile;
+	public GameObject BlueTile2;
+	public GameObject RedTile;
+	public GameObject RedTile2;
+	public Vector2 BoardOffset = new Vector3(0.5f, 0.5f);
+
 	public GameObject Soldier;
 	public GameObject Crop;
 	private bool isCreated;
@@ -21,19 +29,20 @@ public class Board : MonoBehaviour
 
 	// Use this for initialization
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
 		UpdateMouseOver ();
+		//Debug.Log (mouseOver);
 
-		int x = (int)mouseOver.x;
-		int y = (int)mouseOver.y;
+		int x = (int) (mouseOver.x);
+		int y = (int) (mouseOver.y);
 
 		if (Input.GetMouseButtonDown(0))
 		{
+			Debug.Log (x + ", " + y);
 			SelectSoldier (x, y);
 		}
 		/*if (Input.GetMouseButtonUp (0)) 
@@ -53,10 +62,11 @@ public class Board : MonoBehaviour
 		RaycastHit hit;
 		if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
 		{
-			mouseOver.x = (int)(hit.collider.transform.position.x - boardOffset.x);
-			mouseOver.y = (int)(hit.collider.transform.position.y - boardOffset.y);
+			mouseOver.x = (int)(hit.collider.transform.position.x);
+			mouseOver.y = (int)(hit.collider.transform.position.y);
 		}
 	}
+
 
 	void OnTriggerEnter(Collider collider)
 	{
@@ -76,6 +86,7 @@ public class Board : MonoBehaviour
 	{
 		isCreated = false;
 	}
+		
 
 	private void GenerateSoldier(Collider collider, int x, int y)
 	{
@@ -83,13 +94,17 @@ public class Board : MonoBehaviour
 			GameObject newGO = Instantiate (Soldier) as GameObject;
 			newGO.transform.position = new Vector3 (x, y, 0);
 			newGO.gameObject.GetComponent<OneSoldierManager> ().cardAsset = collider.gameObject.GetComponent<OneCardManager> ().cardAsset;
+			newGO.gameObject.GetComponent<SoldierPreview> ().PreviewUnit = collider.gameObject.GetComponent<SoldierPreview> ().PreviewUnit;
+			newGO.gameObject.GetComponent<SoldierPreview> ().PreviewText = collider.gameObject.GetComponent<SoldierPreview> ().PreviewText;
 			//newGO.transform.SetParent (this.gameObject.transform, false);
-			Destroy (collider.gameObject);
 			//Debug.Log (collider.gameObject.GetComponent<OneCardManager> ().cardAsset.name);
 			OneSoldierManager s = newGO.GetComponent<OneSoldierManager>();
 			soldiers[x, y] = s;
 			isCreated = true;
+			Destroy (collider.gameObject);
 		}
+		//if(soldiers[x,y] != null)
+			//Debug.Log (soldiers[x,y].cardAsset.name);
 	}
 
 	private void GenerateCrop(Collider collider, int x, int y)
@@ -98,6 +113,8 @@ public class Board : MonoBehaviour
 			GameObject newGO = Instantiate (Crop) as GameObject;
 			newGO.transform.position = new Vector3 (x, y, 0);
 			newGO.gameObject.GetComponent<OneCropManager> ().cardAsset = collider.gameObject.GetComponent<OneCardManager> ().cardAsset;
+			newGO.gameObject.GetComponent<CropPreview> ().PreviewUnit = collider.gameObject.GetComponent<CropPreview> ().PreviewUnit;
+			newGO.gameObject.GetComponent<CropPreview> ().PreviewText = collider.gameObject.GetComponent<CropPreview> ().PreviewText;
 			//newGO.transform.SetParent (this.gameObject.transform, false);
 			Destroy (collider.gameObject);
 			//Debug.Log (collider.gameObject.GetComponent<OneCardManager> ().cardAsset.name);

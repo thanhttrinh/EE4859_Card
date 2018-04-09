e53ecd5feaa54105b15836b5b93749cf2ed23133
Tuyet Le <tnle833@gmail.com>
2018-04-09 15:30:42 -0500

    enable card command

--- CardGame/Assets/_scripts/_commands/ShowMessageCommand.cs
+++ CardGame/Assets/_scripts/_commands/ShowMessageCommand.cs
@@ -20,8 +20,8 @@ public class ShowMessageCommand : Command{
 		Sequence s = DOTween.Sequence ().AppendInterval(duration).onComplete(() => 
 			{
 				Command.CommandExecutionComplete ();
-			});
-			*/
+			});*/
+
 		Command.CommandExecutionComplete ();
 	}
 }

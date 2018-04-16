b8f5b9e9abbf5ad7a3245ad8af38baee0389e26b
Julian Dane Plaisance <jplais8@lsu.edu>
2018-04-16 14:36:50 -0500

    updated attack and kill script

--- CardGame/Assets/_scripts/attack.cs
+++ CardGame/Assets/_scripts/attack.cs
@@ -3,9 +3,6 @@ using System;
 
 public class attack : MonoBehaviour
 {
-	public Board board = new Board();
-	public GameUnits[,] manager;
-
 	public bool ViableAttack(GameUnits[,] board, int atkx, int atky, int vicx, int vicy)
 	{
 		if(!board[atkx, atky].ValidMove(board, atkx, atky, vicx, vicy))
@@ -18,33 +15,46 @@ public class attack : MonoBehaviour
 		return false;
 	}
 	//Do ViableAttack and have it == true first
-	public void doAttack(GameUnits[,] board, int atkx, int atky, int vicx, int vicy)
+	public void doAttack(Player player, GameUnits[,] board, int atkx, int atky, int vicx, int vicy)
 	{
 		int atkDam = 0, vicDam = 0, atkHP = 0, vicHP = 0;
+        bool isBase = false;
 		Int32.TryParse(board[atkx, atky].GetComponent<OneCardManager>().AttackText.text, out atkDam);
-		Int32.TryParse(board[vicx, vicy].GetComponent<OneCardManager>().AttackText.text, out vicDam);
-		Int32.TryParse(board[atkx, atky].GetComponent<OneCardManager>().HealthText.text, out atkHP);
-		Int32.TryParse(board[vicx, vicy].GetComponent<OneCardManager>().HealthText.text, out vicHP);
+        Int32.TryParse(board[atkx, atky].GetComponent<OneCardManager>().HealthText.text, out atkHP);
 
-		if (atkDam >= vicHP)
+        if (board[vicx, vicy].GetComponent<OneCardManager>() != null)
+        {
+            Int32.TryParse(board[vicx, vicy].GetComponent<OneCardManager>().AttackText.text, out vicDam);
+            Int32.TryParse(board[vicx, vicy].GetComponent<OneCardManager>().HealthText.text, out vicHP);
+        }
+        else
+        {
+            vicHP = board[vicx, vicy].GetComponent<Base>().BaseHP;
+            isBase = true;
+        }
+
+		if (atkDam >= vicHP && !isBase)
 		{
 			board[vicx, vicy] = null;
 		}
-		else if (atkDam < vicHP && vicDam >= atkHP)
+		else if (atkDam < vicHP && vicDam >= atkHP && !isBase)
 		{
 			board[vicx, vicy].GetComponent<OneCardManager>().HealthText.text = (vicHP - atkDam) + "";
 			board[atkx, atky] = null;
 		}
-		else if (atkDam < vicHP && vicDam < atkHP)
+		else if (atkDam < vicHP && vicDam < atkHP && !isBase)
 		{
 			board[vicx, vicy].GetComponent<OneCardManager>().HealthText.text = "" + (vicHP - atkDam);
 			board[vicx, vicy].GetComponent<OneCardManager>().HealthText.text = "" + (atkHP - vicDam);
 		}
-	}
-
-	public attack(Board bd)
-	{
-		board = bd;
-		manager = board.cards;
+        else if(atkDam < vicHP && isBase)
+        {
+            board[vicx, vicy].GetComponent<Base>().BaseHP = vicHP - atkDam;
+        }
+        else if(atkDam >= vicHP && isBase)
+        {
+            board[vicx, vicy].GetComponent<Base>().BaseHP = 0;
+            GameOverCommand game = new GameOverCommand(player);
+        }
 	}
 }

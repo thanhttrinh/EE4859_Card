85bc4ad9eda74fa12bae1d380b2316a14ebbce9e
Thanh Trinh <trinhtania@gmail.com>
2018-04-24 15:30:20 -0500

    add quit game feature
    - player presses the button in the top left
    - will be prompted Yes or No. if yes, goes back to menu scene. if no, resumes game.

---
+++ CardGame/Assets/_scripts/NoQuitButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NoQuitButton : MonoBehaviour {
+
+    public GameObject ConfirmQuitCanvas;
+    public Button NoButton;
+
+    private void Start()
+    {
+        Button no = NoButton.GetComponent<Button>();
+        no.onClick.AddListener(Resume);
+    }
+    public void Resume()
+    {
+        ConfirmQuitCanvas.SetActive(false);
+    }
+}

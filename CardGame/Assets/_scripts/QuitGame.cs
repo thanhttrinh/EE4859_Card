ead88ab6dfafd21cdba2fb5d48ff230b2f061913
Thanh Trinh <trinhtania@gmail.com>
2018-04-24 15:01:16 -0500

    reorganize art assets and add quit game feature
    - in process of adding the quit game feature

---
+++ CardGame/Assets/_scripts/QuitGame.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuitGame : MonoBehaviour {
+
+    public GameObject ConfirmQuitCanvas;
+    public Button QuitButton;
+
+    private void Start()
+    {
+        Button quit = QuitButton.GetComponent<Button>();
+        quit.onClick.AddListener(AreYouSurePrompt);
+    }
+    public void AreYouSurePrompt()
+    {
+        ConfirmQuitCanvas.SetActive(true);
+    }
+}
